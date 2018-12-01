fcc17b0194865176a3f7ea98cc546d063dafcf58
Aziz Ali <45339973+amazingakhlaq@users.noreply.github.com>
2018-12-01 09:50:29 -0500

    Add files via upload

---
+++ Hw2/src/hw2/Computer.java
@@ -0,0 +1,47 @@
+package hw2;
+
+public class Computer {
+	public int ComputerSpace = 400;
+	String computerName = "Dell";
+	public String getComputerName(int ComputerSpace) {
+	return ComputerName;
+	}
+
+	public void Ram() {
+		System.out.println("There is 4GB or RAM");
+}
+	
+	public void HardDisk() {
+		System.out.println("There is 500 GB space in the HDD");
+		}
+	
+	
+	
+	public String ComputerName = "Dell";
+	public String setComputerName(String ComputerName) {
+	return ComputerName;}
+	
+	
+	public int ComputerMemory = 400; 
+	public void getComputerMemory(int computerMemory){
+	this.ComputerMemory = ComputerMemory;
+	
+
+	}
+		
+		public class Computer1 {
+
+			int Memory = 2000;
+			public String Computer1Name = "MyComputer";
+			public String getComputer1Name() {
+		    return Computer1Name;
+			}
+					
+			public void numberOfBedrooms() {
+				System.out.println("There are 5 Bedrooms");
+				
+			}
+}
+}
+
+		
---
+++ Hw2/src/hw2/TestComputer.java
@@ -0,0 +1,10 @@
+package hw2;
+
+public class TestComputer {
+
+	public static void main(String[] args) {
+		Computer Dell = new Computer();
+		Dell.HardDisk();
+		Dell.Ram();
+	}
+}
